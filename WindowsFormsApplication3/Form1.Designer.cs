dc5480538a58b29f1e83db8b0a204770be134cc0
unknown <admin@o-f0bad4a122b64.(none)>
2012-04-10 22:40:27 +0900

    お試し

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "sending";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "learn";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(14, 126);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "timer";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(14, 161);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 28);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(122, 117);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 19);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // button7
+            // 
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(55, 221);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(259, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 25);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(299, 300);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SpeechLib; //for ver11
+using System.Speech.Recognition;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*
+            // ここから追加するコード
+
+            // 音声認識エンジンを生成
+            SpeechRecognitionEngine engine = new SpeechRecognitionEngine();
+
+            // 音声を認識した際に発生するイベントに処理を追加
+            engine.SpeechRecognized += delegate(object sender, SpeechRecognizedEventArgs e)
+            {
+                // 音声認識した結果をフォームのラベルに表示
+                this.label1.Text = e.Result.Text;
+            };
+
+            // 文法読み込み
+            engine.LoadGrammar(new DictationGrammar());
+
+            // 既定のマイク入力をインプットデバイスとして設定
+            engine.SetInputToDefaultAudioDevice();
+            //engine.SetInputToWaveFile(@"C:\Documents and Settings\admin\デスクトップ\sample.wav");
+
+            // 音声認識を非同期で開始
+            engine.RecognizeAsync(RecognizeMode.Multiple);
+
+            // ここまで追加するコード
+            */
+
+
+            LWWS lwws = new LWWS(63); // 63：東京
+            lwws.Refresh();
+            textBox1.Text = lwws.Tenki;
+
+        }
+
+
+        public iRemocon iRem = new iRemocon();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            iRem.connect("192.168.11.6", 51013);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = iRem.check();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = iRem.sending(int.Parse(textBox2.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = iRem.learn(int.Parse(textBox2.Text));
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = iRem.timer(textBox2.Text,long.Parse(textBox3.Text));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = iRem.timerlist();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+        protected override void WndProc(ref Message m)
+        {
+            // Form のドラッグ移動 処理
+            base.WndProc(ref m);
+            if ((m.Msg == 0x84) && (m.Result == (IntPtr)1)) m.Result = (IntPtr)2;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "aaaa";
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            textBox1.Text = "bbbb";
+        }
+    }
+
+
+    public class iRemocon
+    {
+        private System.Net.Sockets.TcpClient objSck = new System.Net.Sockets.TcpClient();
+        private System.Net.Sockets.NetworkStream objStm;
+
+        public void connect(string ip, int port)
+        {
+            // ソケット接続
+            objSck.Connect(ip, port);
+            // ソケットストリーム取得
+            objStm = objSck.GetStream();
+        }
+
+        public string sending(int num)
+        {
+            Byte[] dat = System.Text.Encoding.GetEncoding("SHIFT-JIS").GetBytes("*is;" + num + ";\r\n");
+            objStm.Write(dat, 0, dat.GetLength(0));
+            System.Threading.Thread.Sleep(1000);
+            // ソケット受信
+            if (objSck.Available > 0)
+            {
+                dat = new Byte[objSck.Available];
+                objStm.Read(dat, 0, dat.GetLength(0));
+                return System.Text.Encoding.GetEncoding("SHIFT-JIS").GetString(dat);
+            }
+            return "lost";
+
+        }
+
+        public string check()
+        {
+            Byte[] dat = System.Text.Encoding.GetEncoding("SHIFT-JIS").GetBytes("*au\r\n");
+            objStm.Write(dat, 0, dat.GetLength(0));
+            System.Threading.Thread.Sleep(1000);
+            // ソケット受信
+            if (objSck.Available > 0)
+            {
+                dat = new Byte[objSck.Available];
+                objStm.Read(dat, 0, dat.GetLength(0));
+                return System.Text.Encoding.GetEncoding("SHIFT-JIS").GetString(dat);
+            }
+            return "lost";
+
+        }
+
+        public string learn(int num)
+        {
+
+            Byte[] dat = System.Text.Encoding.GetEncoding("SHIFT-JIS").GetBytes("*ic;" + num + ";\r\n");
+            objStm.Write(dat, 0, dat.GetLength(0));
+            System.Threading.Thread.Sleep(1000);
+            // ソケット受信
+            if (objSck.Available > 0)
+            {
+                dat = new Byte[objSck.Available];
+                objStm.Read(dat, 0, dat.GetLength(0));
+                return System.Text.Encoding.GetEncoding("SHIFT-JIS").GetString(dat);
+            }
+            return "lost";
+        }
+
+        public string timerlist()
+        {
+
+            Byte[] dat = System.Text.Encoding.GetEncoding("SHIFT-JIS").GetBytes("*tl\r\n");
+            objStm.Write(dat, 0, dat.GetLength(0));
+            System.Threading.Thread.Sleep(1000);
+            // ソケット受信
+            if (objSck.Available > 0)
+            {
+                dat = new Byte[objSck.Available];
+                objStm.Read(dat, 0, dat.GetLength(0));
+                return System.Text.Encoding.GetEncoding("SHIFT-JIS").GetString(dat);
+            }
+            return "lost";
+        }
+
+        public string timer(string rcode, long time)
+        {
+            Byte[] dat = System.Text.Encoding.GetEncoding("SHIFT-JIS").GetBytes("*tg\r\n");
+            objStm.Write(dat, 0, dat.GetLength(0));
+            System.Threading.Thread.Sleep(1000);
+            // ソケット受信
+            if (objSck.Available == 0)
+            {
+                return "lost";
+            }
+
+            dat = new Byte[objSck.Available];
+            objStm.Read(dat, 0, dat.GetLength(0));
+            string str = System.Text.Encoding.GetEncoding("SHIFT-JIS").GetString(dat);
+
+            str = str.Substring(6, 10);
+
+            long a = int.Parse(str);
+
+            long b = a + time;
+
+            System.Threading.Thread.Sleep(5000);
+
+            dat = System.Text.Encoding.GetEncoding("SHIFT-JIS").GetBytes("*tm;"+ rcode +";"+b.ToString()+";0\r\n");
+            objStm.Write(dat, 0, dat.GetLength(0));
+            System.Threading.Thread.Sleep(1000);
+            // ソケット受信
+            if (objSck.Available > 0)
+            {
+                dat = new Byte[objSck.Available];
+                objStm.Read(dat, 0, dat.GetLength(0));
+                return System.Text.Encoding.GetEncoding("SHIFT-JIS").GetString(dat);
+            }
+            return "lost";
+        }
+    }
+
+    public class LWWS
+    {
+        // フィールド
+        string _tenki; // 例：曇時々雨
+        string _title; // 例：東京都 東京 - 今日の天気
+        string _link;  // 例：http://weather.livedoor.com/area/13/63.html?v=1
+        bool _available; // データが正しく取得できればtrue
+        DateTime _publicTime; // 予報の発表日時
+        Icon _icon; // タスクトレイ用のアイコン
+
+        // プロパティ
+        public string Tenki { get { return _tenki; } }
+        public string Title { get { return _title; } }
+        public string Link { get { return _link; } }
+        public bool Available { get { return _available; } }
+        public DateTime PublicTime { get { return _publicTime; } }
+        public Icon Icon { get { return _icon; } }
+
+        string lwwsUrl; // お天気WebサービスのURL
+
+        // コンストラクタ
+        public LWWS(int id)
+        {
+            string lwws = "http://weather.livedoor.com/forecast/webservice/rest/v1?city={0}&day=today";
+            lwwsUrl = String.Format(lwws, id);
+        }
+
+        public void Refresh()
+        {
+            _available = false;
+
+            //  XMLデータの取得と解析
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(lwwsUrl);
+            }
+            catch
+            {
+                return;
+            }
+            _title = doc.DocumentElement.SelectSingleNode
+                                           ("/lwws/title").InnerText;
+            _tenki = doc.DocumentElement.SelectSingleNode
+                                           ("/lwws/image/title").InnerText;
+            _link = doc.DocumentElement.SelectSingleNode
+                                           ("/lwws/image/link").InnerText;
+            string imageURL = doc.DocumentElement.SelectSingleNode
+                                           ("/lwws/image/url").InnerText;
+            string pt = doc.DocumentElement.SelectSingleNode
+                                          ("/lwws/publictime").InnerText;
+            _publicTime = DateTime.Parse(pt);
+
+            //  アイコンの作成
+
+            _available = true;
+        }
+    }
+}
